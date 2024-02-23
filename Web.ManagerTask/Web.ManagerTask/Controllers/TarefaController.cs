5187a3552f718cce2006e9bd5ae219b430e21b45
Meselmias Marques <meselmias.marques.lima@gmail.com>
2024-02-22 21:46:56 -0300

    criacao do projeto mvc

---
+++ Web.ManagerTask/Web.ManagerTask/Controllers/TarefaController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyTask.Models;
+
+namespace Web.ManagerTask.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private readonly DbContextImpacta _context;
+
+        public TarefaController(DbContextImpacta context)
+        {
+            _context = context;
+        }
+
+        // GET: Tarefa
+        public async Task<IActionResult> Index()
+        {
+            var dbContextImpacta = _context.Tarefas.Include(t => t.Projeto).Include(t => t.UsuarioResponsavel);
+            return View(await dbContextImpacta.ToListAsync());
+        }
+
+        // GET: Tarefa/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas
+                .Include(t => t.Projeto)
+                .Include(t => t.UsuarioResponsavel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarefa);
+        }
+
+        // GET: Tarefa/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjetoId"] = new SelectList(_context.Projetos, "Id", "Id");
+            ViewData["UsuarioResponsavelId"] = new SelectList(_context.Usuarios, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tarefa/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Descricao,DataCriacao,Status,UsuarioResponsavelId,ProjetoId")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tarefa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjetoId"] = new SelectList(_context.Projetos, "Id", "Id", tarefa.ProjetoId);
+            ViewData["UsuarioResponsavelId"] = new SelectList(_context.Usuarios, "Id", "Id", tarefa.UsuarioResponsavelId);
+            return View(tarefa);
+        }
+
+        // GET: Tarefa/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjetoId"] = new SelectList(_context.Projetos, "Id", "Id", tarefa.ProjetoId);
+            ViewData["UsuarioResponsavelId"] = new SelectList(_context.Usuarios, "Id", "Id", tarefa.UsuarioResponsavelId);
+            return View(tarefa);
+        }
+
+        // POST: Tarefa/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Descricao,DataCriacao,Status,UsuarioResponsavelId,ProjetoId")] Tarefa tarefa)
+        {
+            if (id != tarefa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tarefa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TarefaExists(tarefa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjetoId"] = new SelectList(_context.Projetos, "Id", "Id", tarefa.ProjetoId);
+            ViewData["UsuarioResponsavelId"] = new SelectList(_context.Usuarios, "Id", "Id", tarefa.UsuarioResponsavelId);
+            return View(tarefa);
+        }
+
+        // GET: Tarefa/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas
+                .Include(t => t.Projeto)
+                .Include(t => t.UsuarioResponsavel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarefa);
+        }
+
+        // POST: Tarefa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa != null)
+            {
+                _context.Tarefas.Remove(tarefa);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TarefaExists(int id)
+        {
+            return _context.Tarefas.Any(e => e.Id == id);
+        }
+    }
+}
